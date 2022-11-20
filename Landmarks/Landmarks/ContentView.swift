c4b359fe09e4dc4d6473dd0150533a6d4284439f
Jenny Shalai <jenny.shalai@gmail.com>
2022-11-20 12:40:53 -0500

    Customize the Text View

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Jenny!")
+        Text("Turtle Rock")
             .padding()
+            .font(.title)
+            .foregroundColor(.black)
     }
 }
 
