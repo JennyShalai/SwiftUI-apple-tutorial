ef04908d8e8c082525fcaabb6405072a8635348b
Jenny Shalai <jenny.shalai@gmail.com>
2022-11-20 12:13:27 -0500

    init project

---
+++ SwiftUIFollowAlong.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIFollowAlong.swift
+//  
+//
+//  Created by Jenny Shalai on 11/20/22.
+//
+
+import SwiftUI
+
+struct SwiftUIFollowAlong: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIFollowAlong_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIFollowAlong()
+    }
+}
