ecdd83db0ef53754521d04470a84cc4af195dcb5
Jenny Shalai <jenny.shalai@gmail.com>
2022-11-26 11:23:10 -0500

    Final commets for SwiftUI essentials

--- Landmarks/Landmarks/Views/Landmarks/LandmarkList.swift
+++ Landmarks/Landmarks/Views/Landmarks/LandmarkList.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct LandmarkList: View {
     
     @EnvironmentObject var modelData: ModelData
+    // state for view (Use the @State property wrapper to mark a value as state, declare the property as private, and give it a default value)
     @State private var showFavoritesOnly = true
     
     var filteredLandmarks: [Landmark] {
